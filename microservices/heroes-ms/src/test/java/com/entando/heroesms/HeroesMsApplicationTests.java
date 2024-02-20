63fc85b396591f88ae1254724cb943720feae314
firegloves <lucorset@gmail.com>
2024-02-20 10:53:14 +0100

    bundle init ms init

---
+++ microservices/heroes-ms/src/test/java/com/entando/heroesms/HeroesMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entando.heroesms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
