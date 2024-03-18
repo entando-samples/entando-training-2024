e8f967f2b22390223c0f0b607ae521d73d83c8bb
firegloves <lucorset@gmail.com>
2024-03-18 08:33:48 +0100

    config mfe + keycloak on be

---
+++ microservices/hero-ms/src/main/java/com/entando/heroms/config/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+package com.entando.heroms.config;
+
+import com.entando.heroms.config.jwt.JwtAuthConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfiguration {
+
+    public static final String HEROES_API = "/api/**";
+    public final JwtAuthConverter jwtAuthConverter;
+
+    public SecurityConfiguration(JwtAuthConverter jwtAuthConverter) {
+        this.jwtAuthConverter = jwtAuthConverter;
+    }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable()) //NOSONAR
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers(HttpMethod.OPTIONS, HEROES_API).permitAll()
+                        .requestMatchers(HttpMethod.GET, HEROES_API).permitAll()
+                        .requestMatchers(HttpMethod.POST, HEROES_API).authenticated()
+                        .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers("/error").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter)))
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
